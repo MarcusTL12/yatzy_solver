4d1c3dc5c6eacc5e7100cc5555f26482313ad917
MarcusTL12 <marcuspaafjellet@gmail.com>
2023-04-24 17:50:02 -0400

    error in row 2

---
+++ src/macrosolver/incore.rs
@@ -0,0 +1,67 @@
+// This is a quick draft for the system to build the layers that does not
+// deal with any. This is mainly for testing.
+
+use std::time::Instant;
+
+use ndarray::Array3;
+
+use crate::solver::{solve_layer_type1_5dice, solve_layer_type2_5dice};
+
+type SolvedType = (Array3<Option<Array3<f32>>>, Array3<Option<Array3<u8>>>);
+
+pub fn solve_5dice() -> SolvedType {
+    let mut scores = Array3::from_elem([7, 10, 3], None);
+    let mut strats = Array3::from_elem([7, 10, 3], None);
+
+    for na in (0..7).rev() {
+        for nb in (0..10).rev() {
+            let s0_1 = Some(Array3::zeros([0; 3]));
+            let s0_2 = Some(Array3::zeros([0; 3]));
+            let prev_above_layer_scores = scores
+                .get([na + 1, nb, 2])
+                .unwrap_or(&s0_1)
+                .as_ref()
+                .unwrap();
+            let prev_below_layer_scores = scores
+                .get([na, nb + 1, 2])
+                .unwrap_or(&s0_2)
+                .as_ref()
+                .unwrap();
+
+            println!("na: {na:2}, nb: {nb:2}, nt: 0");
+            let timer = Instant::now();
+
+            let (l_scores, l_strats) = solve_layer_type1_5dice(
+                na,
+                nb,
+                prev_above_layer_scores,
+                prev_below_layer_scores,
+            );
+
+            scores[[na, nb, 0]] = Some(l_scores);
+            strats[[na, nb, 0]] = Some(l_strats);
+
+            let t = timer.elapsed();
+            println!("Took: {t:4.2?}");
+
+            for nt in 1..3 {
+                println!("na: {na:2}, nb: {nb:2}, nt: {nt}");
+                let timer = Instant::now();
+
+                let prev_layer_scores =
+                    scores.get([na, nb, nt - 1]).unwrap().as_ref().unwrap();
+
+                let (l_scores, l_strats) =
+                    solve_layer_type2_5dice(na, nb, prev_layer_scores);
+
+                scores[[na, nb, nt]] = Some(l_scores);
+                strats[[na, nb, nt]] = Some(l_strats);
+
+                let t = timer.elapsed();
+                println!("Took: {t:4.2?}");
+            }
+        }
+    }
+
+    (scores, strats)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 #![feature(array_zip, split_array, generic_const_exprs)]
 #![allow(incomplete_features)]
 
-use std::time::Instant;
-
-use ndarray::Array3;
-use solver::{bottom_layer_dimensions_5dice, solve_layer_type1_5dice};
-
-use crate::solver::solve_layer_type2_5dice;
+use macrosolver::incore::solve_5dice;
 
 pub mod dice_distributions;
 pub mod dice_throw;
@@ -14,32 +9,10 @@ pub mod level_ordering;
 pub mod solver;
 pub mod util;
 pub mod yatzy;
+pub mod macrosolver;
 
 fn main() {
-    let scores0 = Array3::zeros(bottom_layer_dimensions_5dice());
+    let (scores, _strats) = solve_5dice();
 
-    let timer = Instant::now();
-
-    let (scores1, _) =
-        solve_layer_type1_5dice(6, 8, &Array3::zeros([0; 3]), &scores0);
-
-    let d = timer.elapsed();
-    println!("time = {d:?}");
-
-    let timer = Instant::now();
-
-    let (scores2, _) = solve_layer_type2_5dice(6, 8, &scores1);
-
-    let d = timer.elapsed();
-    println!("time = {d:?}");
-
-    let timer = Instant::now();
-
-    let (scores3, strats3) = solve_layer_type2_5dice(6, 8, &scores2);
-
-    let d = timer.elapsed();
-    println!("time = {d:?}");
-
-    println!("{scores3:5.2?}");
-    println!("{strats3:2?}");
+    println!("{}", scores[[0, 0, 2]].as_ref().unwrap());
 }
