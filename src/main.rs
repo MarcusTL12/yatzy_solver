54ac3a44171386113302f23ff0c66ff2bfbb3676
MarcusTL12 <marcuspaafjellet@gmail.com>
2024-01-02 10:46:52 -0500

    expected score

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,28 @@ fn main() {
 
             println!("Expected score for 5 dice: {score:.2}");
         }
+        "expected-score-6x" => {
+            let mut layer = Layer::<6, true> {
+                na: 0,
+                nb: 0,
+                nt: 2,
+                scores: None,
+                strats: None,
+            };
+
+            layer.load_scores();
+            let scores = layer.scores.unwrap();
+
+            let mut score = 0.0;
+
+            for (i, &(_, prob)) in DICE_DISTR.6.iter().enumerate() {
+                let prob = prob as f64 / DICE_DIVISOR[6] as f64;
+
+                score += scores[[0, 0, i]] as f64 * prob;
+            }
+
+            println!("Expected score for 5 dice: {score:.2}");
+        }
         "simulate-5" => {
             let mut scores = vec![0; args[2].parse().unwrap()];
 
